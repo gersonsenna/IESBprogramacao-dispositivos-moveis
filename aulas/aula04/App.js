e251e0a63cc2ba7fb7717a6bfc289618e6459aa2
Gerson <2414290015@academico.iesb.br>
2025-03-25 20:14:44 -0300

    Adicionando arquivos da pratica04

---
+++ aulas/aula04/App.js
@@ -0,0 +1,9 @@
+import DetalhesProduto from "./screens/DetalhesProduto";
+import GaleriaFotos from "./screens/GaleriaFotos";
+import RegistrarConta from "./screens/RegistrarConta";
+
+function App() {
+  return <DetalhesProduto />
+}
+
+export default App;
